6f68b611974a16029b932fe9a3a88604e12e6426
RidwanSiddique44 <srsiddique118@gmail.com>
2022-04-29 16:40:39 +0600

    rout setup completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/sharedPages/Header/Header';
+import Home from './components/mainPages/Home/Home';
+import Notfound from './components/mainPages/Notfound/Notfound';
+import SignIn from './components/registerPages/SignIn/SignIn';
+import SignUp from './components/registerPages/SignUp/SignUp';
+import Blogs from './components/mainPages/Blogs/Blogs';
+import Footer from './components/sharedPages/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* --------header component------------- */}
+      <Header></Header>
+      {/*----------- All routes --------------- */}
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path='/blog' element={<Blogs></Blogs>}></Route>
+        <Route path="/signin" element={<SignIn></SignIn>}></Route>
+        <Route path="/signup" element={<SignUp></SignUp>}></Route>
+        <Route path="*" element={<Notfound></Notfound>}></Route>
+      </Routes>
+      {/*---------- footer component -------------- */}
+      <Footer></Footer>
     </div>
   );
 }
