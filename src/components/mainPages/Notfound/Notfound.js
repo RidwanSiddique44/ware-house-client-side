901b280f6dce8d6ad20aeb5c375af8b0f580a3e6
RidwanSiddique44 <srsiddique118@gmail.com>
2022-04-29 21:15:56 +0600

    notfound page added

--- src/components/mainPages/Notfound/Notfound.js
+++ src/components/mainPages/Notfound/Notfound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './Notfound.css';
+
 
 const Notfound = () => {
     return (
-        <div>
-
+        <div className='bg-body'>
+            <h2 className='mt-5  text-danger fw-bolder'>Opps!! page not found</h2>
         </div>
     );
 };
