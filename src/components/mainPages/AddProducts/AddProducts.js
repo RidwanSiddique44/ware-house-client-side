208956ebfe889131b42637ef923459416a313ffb
RidwanSiddique44 <srsiddique118@gmail.com>
2022-05-05 14:25:26 +0600

    delete method added

--- src/components/mainPages/AddProducts/AddProducts.js
+++ src/components/mainPages/AddProducts/AddProducts.js
@@ -4,18 +4,38 @@ import { useForm } from "react-hook-form";
 
 const AddProducts = () => {
     const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data);
+    const onSubmit = data => {
+        const url = `http://localhost:5000/products`;
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(result => {
+                console.log(result)
+            })
+    };
     return (
-        <div className='container text-start mt-5 mb-5 w-50 ms-auto'>
+        <div className='container border border-danger p-5 text-start mt-5 mb-5 w-75 ms-auto'>
             <h3 className="text-center">Add Products</h3>
+            <hr className="bg-danger" />
             <form className='d-flex flex-column' onSubmit={handleSubmit(onSubmit)}>
                 <label>Product Name</label>
-                <input className='mb-3'  {...register("firstName", { required: true, maxLength: 20 })} />
+                <input placeholder='Name' className='mb-3'  {...register("name", { required: true, maxLength: 20 })} />
+                <label>Supplier Name</label>
+                <input placeholder='Supplier' className='mb-3'  {...register("supplier", { required: true, maxLength: 20 })} />
                 <label>Product Image</label>
-                <input className='mb-3'  {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
+                <input placeholder='Photo url' className='mb-3'  {...register("image")} />
                 <label>Product Quantity</label>
-                <input className='mb-3' type="number" {...register("age", { min: 18, max: 99 })} />
-                <input className='mb-3 bg-primary text-light fw-bolder' type="submit" />
+                <input placeholder='Quantity' className='mb-3' type="number" {...register("quantity")} />
+                <label>Product Price</label>
+                <input placeholder='Price' className='mb-3' type="number" {...register("price")} />
+                <label>Product Description</label>
+                <textarea placeholder='Description' className='mb-3'  {...register("description")} />
+                <input className='mb-3 bg-danger py-2 text-light fw-bolder' type="submit" />
             </form>
         </div>
     );
--- src/components/mainPages/Inventory/Inventory.js
+++ src/components/mainPages/Inventory/Inventory.js
@@ -1,15 +1,53 @@
 import React from 'react';
+import './Inventory.css';
+import { Button, Card } from 'react-bootstrap';
 import useProducts from '../../Hooks/useProducts';
-import AllProducts from './AllProducts/AllProducts';
 
 const Inventory = () => {
     const [products, setProducts] = useProducts();
+    const handleDelete = id => {
+        const confirm = window.confirm('Are you sure to detele it?');
+        if (confirm) {
+            const url = `http://localhost:5000/products/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    const remainingProducts = products.filter(item => item._id !== id);
+                    setProducts(remainingProducts)
+                })
+        }
+    }
+
     return (
-        <div>
-            <div className=' container review-container'>
+        <div className='my-5 py-5'>
+            <div className=' container inventory-style'>
 
                 {
-                    products.map(product => <AllProducts key={product._id} product={product} ></AllProducts>)
+                    products.map(product =>
+                        <div className='card-container'>
+                            <div>
+
+                                <Card style={{ width: '100%' }}>
+                                    <Card.Img className='card-img' style={{ width: '100%' }} variant="top" src={product.image} />
+                                    <Card.Body>
+                                        <Card.Title>{product.name}</Card.Title>
+                                        <Card.Text>
+                                            <span className='fw-bolder'> price: {product.price}</span>
+                                        </Card.Text>
+                                        <Card.Text>
+                                            <span className='fw-bolder'>Description:</span>
+                                            {product.description}
+                                        </Card.Text>
+                                        <Button onClick={() => handleDelete(product._id)} variant="primary">Delete</Button>
+                                    </Card.Body>
+                                </Card>
+                            </div>
+                        </div>
+
+                    )
                 }
             </div>
         </div>
